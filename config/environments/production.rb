6269aceee44459d08644b46e7f1332b1bf8671f0
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-07-27 21:23:01 -0700

    action mailer settings 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,11 +52,11 @@ Myflix::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'myflix88',
-    password:             'tw3ntytw3nty',
-    authentication:       'plain',
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'myflix88',
+    password: 'tw3ntytw3nty',
+    authentication: 'plain',
     enable_starttls_auto: true }
   config.action_mailer.perform_deliveries = true
 
