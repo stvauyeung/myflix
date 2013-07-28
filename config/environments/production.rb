cdd0328a949ecbbd6f7eb861c71e308196de0fbc
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-07-27 20:38:07 -0700

    update production config 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'www.gmail.com',
     user_name:            'myflix88',
     password:             'tw3ntytw3nty',
     authentication:       'plain',
