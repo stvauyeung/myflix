cc27311090a15c5fdf067129fe3ca107dcd86406
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-07-31 22:48:02 -0700

    refactor reset password spec

--- spec/features/reset_password_spec.rb
+++ spec/features/reset_password_spec.rb
@@ -2,17 +2,52 @@ require 'spec_helper'
 
 feature "User resets password" do
 	background do 
-		Fabricate(:user, email: "molly@molly.com", password: "password")
+		Fabricate(:user, name: "Molly", email: "molly@molly.com", password: "password")
 	end
 
 	scenario "reseting user password" do
 		visit '/login'
-		
-		click_link 'Forgot Password?'
-		expect(page).to have_content "We will send you an email with a link that you can use to reset your password."
+		click_forgot_password		
 
-		fill_in 'email', with: 'molly@molly.com'
+		fill_forgot_password_form('molly@molly.com')		
+		check_password_reset_email('molly@molly.com')
+		click_email_link
+				
+		fill_new_password_form('new_password')
+		login_with_new_password('new_password')		
+	end
+
+	def click_forgot_password
+		click_link 'Forgot Password?'
+		expect(page).to have_content 'We will send you an email with a link that you can use to reset your password.'	
+	end
+
+	def fill_forgot_password_form(email)
+		fill_in 'email', with: email
 		click_button 'Reset Password'
-		expect(page).to have_content "We have send an email with instruction to reset your password."
+		page.should have_content 'We have send an email with instruction to reset your password.'
+	end
+
+	def check_password_reset_email(email)
+		open_email(email)
+		current_email.should have_content "Please click the link below to reset your password"
+	end
+
+	def click_email_link
+		current_email.click_link 'Reset your Myflix Password'
+		page.should have_content 'New Password'	
+	end
+
+	def fill_new_password_form(new_password)
+		fill_in 'password', with: new_password
+		click_button 'Reset Password'
+		page.should have_content 'Your password has been reset'	
+	end
+
+	def login_with_new_password(new_password)
+		fill_in 'email', with: 'molly@molly.com'
+		fill_in 'password', with: new_password
+		click_button 'Login'
+		page.should have_content 'Welcome back, Molly'
 	end
 end
