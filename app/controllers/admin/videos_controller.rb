a8a06acdad3a8fd1f0ed1fd94e540b906eedc18b
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-08-12 18:15:03 -0700

    set up admin new video controller

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -7,7 +7,15 @@ class Admin::VideosController < ApplicationController
 	end
 
 	def create
-		
+		@video = Video.new(params[:video].except(:categories))
+		if @video.save
+			@video.categories << Category.find(params[:video][:categories])
+			flash[:success] = "You successfully added #{@video.title}."
+			redirect_to new_admin_video_path
+		else
+			flash[:error] = "Please fix the following errors."
+			render :new
+		end
 	end
 
 	private
