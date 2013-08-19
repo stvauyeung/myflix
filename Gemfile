dee3d0a579443c1cbb8e9cbbb132a6e9151dda34
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-08-18 23:30:46 -0700

    add vcr

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ group :test do
   gem 'capybara'
   gem 'capybara-email'
   gem 'launchy'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development, :test do
--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -13,7 +13,7 @@ describe StripeWrapper::Charge do
 		).id
 	end
 
-	context "with valid card" do
+	context "with valid card", :vcr do
 		let(:card_number) { '4242424242424242' }
 		it "charges the card successfully" do
 			response = StripeWrapper::Charge.create(amount: 300, card: token, description: 'test charge')
@@ -24,10 +24,10 @@ describe StripeWrapper::Charge do
 	context "with invalid card" do
 		let(:card_number) { '4000000000000002' }
 		let(:response) { StripeWrapper::Charge.create(amount: 300, card: token, description: 'test charge') }
-		it "does not charge the card successfully" do
+		it "does not charge the card successfully", :vcr do
 			response.should be_failed
 		end
-		it "contains an error message" do
+		it "contains an error message", :vcr do
 			response.error_message.should be_present
 		end
 	end
